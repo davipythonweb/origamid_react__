f93e650ee0ea0cf2cec184ecb04de4aeaf6a337d
davypythonweb <galaxys3tab32@gmail.com>
2024-10-30 14:38:41 -0300

    create exemplo - aula 616

--- class_616/src/App.jsx
+++ class_616/src/App.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import viteLogo from '/vite.svg'
 
 function App() {
+  const [contar, setContar] = React.useState(0)
 
-  return 
+  React.useEffect( () => {
+    console.info('Executou')
+  })
+
+  return <button className='btn btn-primary ' onClick={() => setContar(contar + 1)}>{contar}</button>
 }
 
  
