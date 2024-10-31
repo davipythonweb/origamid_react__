e0937d2ed9cd81f088483e7742d716fffb18d1f9
davypythonweb <galaxys3tab32@gmail.com>
2024-10-31 18:13:38 -0300

    create exercicio - aula 617 - userEffect

--- class_617/Exercicio.js
+++ class_617/Exercicio.js
@@ -1,1 +1,11 @@
-// Quando o usuario clicar em um dos botoes, faça
+// Crie dois botoes: Notebook e Smartphone e mostre as informaçoes do produto.
+
+// Quando o usuario clicar em um dos botoes, faça um fetch do produto que 
+// foi clicado utilizando a api abaixo:
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+
+// Mostre o nome e o preço na tela ( separe essa informaçao em um componente Produto.jsx)
+
+// Defina o produto clicando como uma preferencia do usuario no localstorage
+// Quando o usuario entrar no site, se existir um produto no localstorage, faça o fetch do mesmo.
