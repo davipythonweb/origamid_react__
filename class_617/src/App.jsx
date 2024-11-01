1ae5c53dfe9d0ff31d2b4642672c0c3ea0f5eb9c
davypythonweb <galaxys3tab32@gmail.com>
2024-10-31 23:35:16 -0300

    exercicio - aula 617 - useEffect

--- class_617/src/App.jsx
+++ class_617/src/App.jsx
@@ -1,11 +1,23 @@
-import react from 'react'
+import React from 'react'
 import viteLogo from '/vite.svg'
 
 
 const App = () => {
   
+const [produto, setProduto] = React.useState(null)
 
-  return <div></div>
+function handleClick( { target }) {
+  setProduto(target.innerText)
+}
+
+  return (
+
+    <div>
+      <h1>Preferência: {produto}</h1>
+      <button onClick={handleClick} style={{marginRight: '1rem'}}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+    </div>
+  )
 }
 
 export default App
