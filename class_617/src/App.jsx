21b5e0b387c05c5e49f8ebbc0a3825393595ce51
davypythonweb <galaxys3tab32@gmail.com>
2024-11-01 01:10:49 -0300

    update exercicio - aula 617 - userEffect

--- class_617/src/App.jsx
+++ class_617/src/App.jsx
@@ -4,29 +4,42 @@ import Produto from './Produto'
 
 
 const App = () => {
-  
+
+  // produto que ira ser referenciado, quando for clicado
 const [produto, setProduto] = React.useState(null)
 
+// efeito para definir apenas uma vez só quando o site inicia
 React.useEffect(() => {
   const produtoLocal = window.localStorage.getItem('produto')
+  // verificar se o efeito nao existir
   if (produtoLocal !== null) setProduto(produtoLocal)
   
 }, [])
 
+// sanvando (a preferencia) no localStorage  do browser com Hook userEffect
 React.useEffect(() => {
+  // define o localStorage se for diferente de null
   if (produto !== null) window.localStorage.setItem('produto', produto)
+    // a dependencia será o produto, sempre que o produto for modificado, o efeito sera acionando
 }, [produto])
 
+// funçao que eh chamada com o click nos botoes , acionando o evento
 function handleClick( { target }) {
+  // prorpiedade para pegar o texto de dentro do produto
   setProduto(target.innerText)
 }
 
   return (
 
     <div>
+      {/* pegando o produto e exibindo */}
       <h1>Preferência: {produto}</h1>
+
+      {/* botoes com as funçoes => onClick */}
       <button onClick={handleClick} style={{marginRight: '1rem'}} className='btn btn-outline-danger'>notebook</button>
       <button onClick={handleClick} className='btn btn-outline-danger'>smartphone</button>
+
+      {/* chamando o componete Produto e atribuindo a preferencia (produto) */}
       <Produto produto={produto} />
     </div>
   )
--- class_617/src/Produto.jsx
+++ class_617/src/Produto.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 
 const Produto = ({produto}) => {
+    // definindo um estado como reativo
   const [dados, setDados] = React.useState(null)
 
+//   fazendo o fetch com o efeito userEffect
   React.useEffect(() => {
+    // verificando se o produto eh diferente de null, para executar o efeito
     if (produto !== null)
+        // fazendo o fetch na API  e já passando o valor do produto
         fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
+            // transformando os dados da API da resposta para json 
             .then((response) => response.json())
+            // mudando o estado dos dados para json
             .then((json) => setDados(json))
+    // chamando a dependencia de produto
   }, [produto])
 
+//   verifica se os dados forem diferentes de null, retorna os dados de baixo
   if (dados === null) return null
     return (
         <div>
+            {/* pegado os dados  */}
             <h1>{dados.nome}</h1>
             <h1>R$ {dados.preco}</h1>
         </div>
