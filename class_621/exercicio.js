29fc9b43de3bed01d0ae15465005936d0a81a756
davypythonweb <galaxys3tab32@gmail.com>
2024-11-06 17:19:31 -0300

    create exercicio aula 621 - useContext

---
+++ class_621/exercicio.js
@@ -0,0 +1,10 @@
+// Exercicio useContext
+
+
+// Utilize o GlobalContext do exemplo anterior para puxar
+// os dados da API abaixo:
+// https://ranekapi.origamid.dev/json/api/produto/
+// assim que o usuario acessar o app
+// coloque os dados da API  em estado reativo e no contexto global, dando acesso aos dados da mesma
+// defina uma fução chamada limparDadis, que eh responsavel por zerar os dados de produto
+// e exponha essa funcao no contexto global
